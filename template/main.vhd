e41b515d64032eb3efac4779dfd17004028c4f23
Tim 'mithro' Ansell <mithro@mithis.com>
2015-07-19 23:58:58 +1000

    Temp commit.

--- template/main.vhd
+++ template/main.vhd
@@ -3,10 +3,17 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity opsis is
   Port ( switch : in  STD_LOGIC;
-         LED : out  STD_LOGIC);
+         LED0 : out  STD_LOGIC;
+			LED1 : out  STD_LOGIC;
+			LED2 : out  STD_LOGIC;
+			LED3 : out  STD_LOGIC
+			);
 end opsis;
 
 architecture Behavioral of opsis is
 begin
-  LED <= switch;
+  LED0 <= switch;
+  LED1 <= switch;
+  LED2 <= switch;
+  LED3 <= switch;
 end Behavioral;
